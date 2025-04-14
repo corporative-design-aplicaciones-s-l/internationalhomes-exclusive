92a1dbd7b4b43cc446e398ef89b19e58dd73cde1
Trollkopf <max.serratosa@gmail.com>
2025-04-14 15:11:16 +0200

    updated to production

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -47,6 +47,8 @@
 
     @stack('scripts')
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
     <script>
         AOS.init();
     </script>
