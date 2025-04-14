7f572d5dc9048f8ac0075bfbc307c1e060c83a81
Trollkopf <max.serratosa@gmail.com>
2025-04-14 15:20:10 +0200

    updated to production

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index']);
 
 
-Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
