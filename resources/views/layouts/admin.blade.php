4751c3c8a511396212299fa7fb903a662f843fdc
Trollkopf <max.serratosa@gmail.com>
2025-04-14 15:19:42 +0200

    updated to production

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @yield('styles') <!-- Aquí cargamos los estilos específicos de cada vista -->
 
@@ -172,7 +178,8 @@
                                     <li class="nav-item"><a class="nav-link"
                                             href="{{ route('admin.properties.index') }}">Propiedades</a>
                                     </li>
-                                    <li class="nav-item"><a class="nav-link" href="{{ route('admin.zonas.index') }}">Zonas</a>
+                                    <li class="nav-item"><a class="nav-link"
+                                            href="{{ route('admin.zonas.index') }}">Zonas</a>
                                     </li>
                                     <li class="nav-item"><a class="nav-link" href="#">Contactos</a></li>
                                 </ul>
